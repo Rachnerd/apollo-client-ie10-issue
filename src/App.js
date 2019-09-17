685d71b739655452ef9bcb3d0a4aa4b0cea608d3
Rachnerd <rachel_149@hotmail.com>
2019-09-17 14:04:12 +0200

    Reproduced IE10 issue.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ApolloClient, {gql, InMemoryCache} from 'apollo-boost';
+import fetch from 'unfetch';
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    fetch,
+    cache,
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    resolvers: {
+        Query: {
+            visibilityFilter: () => {
+                return "SHOW_ALL";
+            }
+        }
+    }
+});
+
+client.query({
+    query: gql`
+        {
+            visibilityFilter @client
+        }
+    `
+})
+.then(result => console.log(JSON.stringify(result)));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <p>Check console f12.</p>
+            <p>Chrome: {`{"data":{"visibilityFilter":"SHOW_ALL"},"loading":false,"networkStatus":7,"stale":false}`}</p>
+            <p>IE10: <span style={{color: 'red'}}>{`Uncaught (in promise)Error: Network error: Object doesn't support property or method 'of'`}</span>
+            </p>
+            <p>IE11: {`{"data":{"visibilityFilter":"SHOW_ALL"},"loading":false,"networkStatus":7,"stale":false}`}</p>
+        </>
+    );
 }
 
 export default App;
